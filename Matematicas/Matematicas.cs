a70056fb3f1edb834b7cdbb113c6c9414006a64e
alhambrx <alhambraespigares@gmail.com>
2026-01-04 19:14:42 +0100

    TDD Red: añadidos tests de EsPerfecto, sin implementar

---
+++ Matematicas/Matematicas.cs
@@ -0,0 +1,9 @@
+﻿    public class Matematicas
+    {
+        // Función para verificar si un num es perfecto
+        public bool EsPerfecto(int n)
+        {
+        throw new NotImplementedException();
+    }
+}
+
