74f09a56556ef2fdfade526699cd5f3e30a72279
alhambrx <alhambraespigares@gmail.com>
2026-01-04 19:17:55 +0100

    TDD Green: implementado EsPerfecto, tests pasan

--- Matematicas/Matematicas.cs
+++ Matematicas/Matematicas.cs
@@ -3,7 +3,13 @@
         // Función para verificar si un num es perfecto
         public bool EsPerfecto(int n)
         {
-        throw new NotImplementedException();
+            int suma = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                    suma += i;
+            }
+            return suma == n;
+        }
     }
-}
 
