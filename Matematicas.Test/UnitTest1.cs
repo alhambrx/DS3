93a04587008329751e33cbb254481f0647559bcf
alhambrx <alhambraespigares@gmail.com>
2026-01-04 21:12:21 +0100

    Fix code formatting (dotnet format)

--- Matematicas.Test/UnitTest1.cs
+++ Matematicas.Test/UnitTest1.cs
@@ -1,18 +1,18 @@
 using Xunit;
 
 public class MatematicasTests
+{
+    [Fact]
+    public void EsPerfecto_6_RetornaTrue()
     {
-        [Fact]
-        public void EsPerfecto_6_RetornaTrue()
-        {
-            var m = new Matematicas();
-            Assert.True(m.EsPerfecto(6)); // 6 es perfecto
-        }
-
-        [Fact]
-        public void EsPerfecto_8_RetornaFalse()
-        {
-            var m = new Matematicas();
-            Assert.False(m.EsPerfecto(8)); // 8 no es perfecto
-        }
+        var m = new Matematicas();
+        Assert.True(m.EsPerfecto(6)); // 6 es perfecto
     }
+
+    [Fact]
+    public void EsPerfecto_8_RetornaFalse()
+    {
+        var m = new Matematicas();
+        Assert.False(m.EsPerfecto(8)); // 8 no es perfecto
+    }
+}
--- Matematicas/Matematicas.cs
+++ Matematicas/Matematicas.cs
@@ -1,15 +1,15 @@
-﻿    public class Matematicas
+﻿public class Matematicas
+{
+    // Función para verificar si un num es perfecto
+    public bool EsPerfecto(int n)
     {
-        // Función para verificar si un num es perfecto
-        public bool EsPerfecto(int n)
+        int suma = 0;
+        for (int i = 1; i < n; i++)
         {
-            int suma = 0;
-            for (int i = 1; i < n; i++)
-            {
-                if (n % i == 0)
-                    suma += i;
-            }
-            return suma == n;
+            if (n % i == 0)
+                suma += i;
         }
+        return suma == n;
     }
+}
 
